fdf2996f42b7badaf9754c9ae22de7b5bf889a88
Cold Dev <96265196+icemedia001@users.noreply.github.com>
2023-02-21 23:07:04 +0100

    Updated Portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ==== typing animation ==== */
+var typed = new Typed(".typing",
+{
+    strings:["Backend Web Developer", "Genz", "Graphics Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+});
